9f47e3c3e591c929e32205942a211b9e45035081
Robert Li <dr.robert.li.au@gmail.com>
2024-12-08 19:54:43 +1000

    modify deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
